6a0f9df93f91fcaaad01bbd1e31dc34104031304
nanagims <61430489+nanagims@users.noreply.github.com>
2022-03-17 18:47:03 +0100

    c script and program

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ i = sizeof(int);
 li = sizeof(long int);
 lli = sizeof(long long int);
 f = sizeof(float);
-printf("size of char: %d byte(s)\n", c);
-printf("size of int: %d byte(s)\n", i);
-printf("size of long int: %d byte(s)\n", li);
-printf("size of long long int: %d byte(s)\n", lli);
-printf("size of float: %d byte(s)\n", f);
+printf("Size of a char: %d byte(s)\n", c);
+printf("Size of a int: %d byte(s)\n", i);
+printf("Size of a long int: %d byte(s)\n", li);
+printf("Size of a long long int: %d byte(s)\n", lli);
+printf("Size of a float: %d byte(s)\n", f);
 return (0);
 }
